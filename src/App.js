98a9f0a766047d93ad183b092d09f11b65df47a5
Hyeonii <63392456+Hyeonii@users.noreply.github.com>
2021-05-03 23:07:51 +0900

    Change header title using useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 
-function App() {
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  // 리액트 라이프사이클을 이해하면 함수 호출 타이밍을 잘 정할 수 있다
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
+
+const App = () => {
+  // 함수를 호출하는 방법이 참 다양하군
+  const titleUpdater = useTitle("Loading...");
+  // setTimeout을 사용하여 함수를 실행시킬 시간을 정할 수 있다.
+  setTimeout(() => titleUpdater("Home"), 3000);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Hi</div>
     </div>
   );
-}
+};
 
 export default App;
